3f99e4f714e159c1f7b7448f8a75ddeecfe2eab3
boa.christians <boa.christians@localhost>
2012-04-23 22:48:50 +0200

    test

---
+++ framework/playground/playground/LabelSearcher.java
@@ -0,0 +1,72 @@
+/*
+ * LabelSearcher.java
+ * 
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 3 of the License, or (at your
+ * option) any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+package playground;
+
+import java.io.File;
+import java.util.Set;
+
+import playground.BoaAnnotation.Type;
+
+public abstract class LabelSearcher
+{
+	private static Set<String> surfaceForms;
+	private static Set<String> falseSurfaceForms;
+	private static Type annoType;
+	protected static SearchAlgorithm algoSimple;
+	protected static SearchAlgorithm algoHard;
+	
+	public LabelSearcher(Type annoType, String configPath)
+	{
+		try
+		{
+			LabelSearcher.annoType = annoType;
+			
+			surfaceForms = new ConfigLoader().openConfigSurfaceForms(new File(configPath), false);
+			falseSurfaceForms = new ConfigLoader().openConfigSurfaceForms(new File(configPath), true);
+		}
+		catch(Exception e)
+		{
+			//do sth
+		}
+	}
+	
+	public void searchUnit(BoaSentence sentence)
+	{
+		try
+		{
+			algoSimple.search(sentence, surfaceForms, annoType);
+		}
+		catch(Exception e)
+		{
+			//do sth
+		}
+		
+		return;
+	}
+	public void searchFalseUnit(BoaSentence sentence)
+	{
+		try
+		{
+			algoHard.search(sentence, falseSurfaceForms, annoType);
+		}
+		catch(Exception e)
+		{
+			//do sth
+		}
+		
+		return;
+	}
+}
---
+++ framework/playground/playground/SearchAlgorithm.java
@@ -0,0 +1,85 @@
+/*
+ * SearchAlgorithm.java
+ * 
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 3 of the License, or (at your
+ * option) any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+package playground;
+
+import java.util.Set;
+
+import playground.BoaAnnotation.Type;
+
+public abstract class SearchAlgorithm
+{
+	//subclasses will have to overwrite this
+	public void search(BoaSentence sentence, Set<String> surForms, Type annoType)
+	{
+		return;
+	}
+	
+	/**
+	 * Checks if a String representation of a token is a number
+	 * @param token
+	 * @return true if it can be parsed to an integer or double value, else false
+	 */
+	
+	
+	public boolean checkIfNumber(String token)
+	{	
+		try
+		{
+			Integer.parseInt(token);
+			return true;
+		}
+		catch(NumberFormatException e){}
+		try
+		{
+			if(token.contains(",")) token = token.replace(",", ".");
+			Double.parseDouble(token);
+			return true;
+		}
+		catch(NumberFormatException e){}
+				
+		return false;
+	}
+	
+	
+	/**
+	 * Checks if a String representation of a token starts with a number.
+	 * @param token
+	 * @return -1 if no combined number else position of first non-numerical char
+	 */
+	
+	public int checkIfStartsWithNumber(String token)
+	{
+		String prefix;
+		int length = 1;
+		int position = -1;
+		
+		try
+		{
+			while(length < token.length())
+			{
+				prefix = token.substring(0, length);
+				length++;
+				
+				if(checkIfNumber(prefix)) position = length;
+			}
+		}
+		catch(NumberFormatException e){}
+		
+		return position;
+	}
+	
+
+}
