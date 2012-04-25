e2adc16cbd4bdfe9f0d2b92ca3a9356bf0b60fbd
boa.jakob <boa.jakob@localhost>
2012-04-25 11:42:38 +0200

    the test class now works with the LabelSearchers. However they somehow tend to use the same variables (has to be due to using the constructor of super class). still needs to be fixed

--- framework/playground/playground/TestClass.java
+++ framework/playground/playground/TestClass.java
@@ -18,16 +18,16 @@ package playground;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.Iterator;
-import java.util.Set;
-
 import nu.xom.ParsingException;
 import nu.xom.ValidityException;
 
 import de.uni_leipzig.informatik.pcai042.boa.BoaAnnotation.Type;
 import de.uni_leipzig.informatik.pcai042.boa.BoaSentence;
-import de.uni_leipzig.informatik.pcai042.boa.ConfigLoader;
+import de.uni_leipzig.informatik.pcai042.boa.LabelSearcher;
+import de.uni_leipzig.informatik.pcai042.boa.LabelSearcherLinearMeasure;
+import de.uni_leipzig.informatik.pcai042.boa.LabelSearcherTemperature;
+import de.uni_leipzig.informatik.pcai042.boa.LabelSearcherWeight;
 import de.uni_leipzig.informatik.pcai042.boa.NaiveAlgorithm;
 import de.uni_leipzig.informatik.pcai042.boa.SearchAlgorithm;
 import de.uni_leipzig.informatik.pcai042.boa.SentenceLoader;
@@ -61,52 +61,10 @@ public class TestClass
 		//create Algo
 		SearchAlgorithm testAlgo = new NaiveAlgorithm();
 		
-		//create HashSets of surfFroms to search for
-		//this will be replaced as soon as ConfigLoader works
-		//1: LINEAR_MEASURE
-		Set<String> testSetLM = new HashSet<String>();
-		testSetLM.add("meters");
-		testSetLM.add("metres");
-		testSetLM.add("miles");
-		testSetLM.add("yards");
-		testSetLM.add("kilometers");
-		testSetLM.add("kilometres");
-		testSetLM.add("km");
-		testSetLM.add("m");
-		testSetLM.add("millimeters");
-		testSetLM.add("millimetres");
-		testSetLM.add("mm");
-		testSetLM.add("centimeters");
-		testSetLM.add("centimetres");
-		testSetLM.add("cm");
-		testSetLM.add("nanometers");
-		testSetLM.add("nanometres");
-		testSetLM.add("nm");
-		testSetLM.add("inches");
-		testSetLM.add("ft");
-		testSetLM.add("feet");
-		
-		//2: WEIGHT
-		Set<String> testSetW = new HashSet<String>();
-		testSetW.add("kg");
-		testSetW.add("kilogram");
-		testSetW.add("g");
-		testSetW.add("gram");
-		testSetW.add("tonnes");
-		testSetW.add("ounze");
-		testSetW.add("pounds");
-		testSetW.add("labs");
-		testSetW.add("lbs");
-		
-		//3: TEMPERATURE
-		Set<String> testSetT = new HashSet<String>();
-		testSetT.add("Kelvin");
-		testSetT.add("K");
-		testSetT.add("K.");
-		testSetT.add("°C");
-		testSetT.add("°C.");
-		testSetT.add("°F");
-		testSetT.add("°F.");
+		//create Searchers
+		LabelSearcher lsLinMeasure = new LabelSearcherLinearMeasure(Type.LINEAR_MEASURE,"SF_LINEAR_MEASURE.properties");
+		LabelSearcher lsWeight = new LabelSearcherWeight(Type.WEIGHT,"SF_WEIGHT.properties");
+		LabelSearcher lsTemp = new LabelSearcherTemperature(Type.TEMPERATURE, "SF_TEMPERATURE.properties");
 		
 		Iterator<BoaSentence> it = sentences.iterator();
 		
@@ -120,14 +78,11 @@ public class TestClass
 			
 			System.out.println("\nSentence " + count + "\n" + testSentence);
 		
-			testAlgo.search(testSentence, testSetLM, Type.LINEAR_MEASURE);
-			testAlgo.search(testSentence, testSetW, Type.WEIGHT);
-			testAlgo.search(testSentence, testSetT, Type.TEMPERATURE);
+			//lsLinMeasure.searchUnit(testSentence);
+			lsWeight.searchUnit(testSentence);
+			//lsTemp.searchUnit(testSentence);
 			System.out.println(testSentence.getAnnotations().toString());
 			
 		}
-		
-		//System.out.println("400km " + testAlgo.checkIfCombinedNumber("400km") + "\n1a" + testAlgo.checkIfCombinedNumber("1a")); 
 	}
-
 }
