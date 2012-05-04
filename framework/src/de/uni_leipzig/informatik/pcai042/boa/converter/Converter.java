bbd01ce1d649ca50cf5added2c1a24f62f709edb
boa.simons <boa.simons@localhost>
2012-05-04 15:11:01 +0200

    Merge with 088c5ccc58453daa8ec1846663d8081cbf3f2d66

--- framework/src/de/uni_leipzig/informatik/pcai042/boa/converter/Converter.java
+++ framework/src/de/uni_leipzig/informatik/pcai042/boa/converter/Converter.java
@@ -15,11 +15,18 @@
 
 package de.uni_leipzig.informatik.pcai042.boa.converter;
 
+import java.io.File;
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import nu.xom.ParsingException;
+import nu.xom.ValidityException;
+
 import de.uni_leipzig.informatik.pcai042.boa.manager.BoaAnnotation;
+import de.uni_leipzig.informatik.pcai042.boa.manager.ConfigLoader;
+import de.uni_leipzig.informatik.pcai042.boa.manager.SentenceLoader;
 
 /**
  * 
@@ -27,23 +34,20 @@ import de.uni_leipzig.informatik.pcai042.boa.manager.BoaAnnotation;
  */
 public abstract class Converter
 {
-	private HashMap<String, BigDecimal> conversionStandard;
-	private HashMap<String, BigDecimal> conversionUnit;
 	
+	private static HashMap<String, BigDecimal> conversionStandard;
+	private static HashMap<String, BigDecimal> conversionUnit;
 	/**
 	 * 
 	 * 
 	 * @param token
 	 * @param number
 	 */
-	public Converter(String token, BigDecimal number)
+
+	public Converter(String token, double number)
 	{
-		// TODO Auto-generated constructor stub
-	}
 	
-	/**
-	 * 
-	 */
+	}	
 	public Converter()
 	{
 		
@@ -60,12 +64,120 @@ public abstract class Converter
 		// null --> placeholder for return
 		return null;
 	}
-	
+
 	/**
 	 * Test
 	 */
 	public static void main(String arg[])
 	{
+		
+		
+		
+		int test=0;
+		String []g={"g","gram"};
+		String []t={"t","ton","metric ton","metric tons","metric tons"};
+		String []kg={"kg","kilogram"};
+		SentenceLoader s1;
+		double f;
+		s1=null;
+		try
+		{
+			s1 = new SentenceLoader(new File("goldstandard.xml"));
+		} catch (ValidityException e)
+		{
+			e.printStackTrace();
+		} catch (ParsingException e)
+		{
+			e.printStackTrace();
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		
+		if (s1 == null)
+			return;
+		
+		
+		for (int i = 0; i < s1.getSentenceCount(); i++)
+		{
+			
+			
+				for (int k = 0; k < s1.getSentence(i).getAnnotations().size(); k++)
+				{
+					String help2=null;
+					if(s1.getSentence(i).getAnnotations().get(k).getType().toString()!="DATE"&&s1.getSentence(i).getAnnotations().get(k).getType().toString()!="TEMPERATURE")
+					{	
+						conversionStandard  = new ConfigLoader().openConfigConversion("Standard", s1.getSentence(i).getAnnotations().get(k).getType().toString());
+						conversionUnit  = new ConfigLoader().openConfigConversion("Unit", s1.getSentence(i).getAnnotations().get(k).getType().toString());
+					}	
+				
+					//System.out.println(conversionStandard);
+					for (int j = 0; j < s1.getSentence(i).getAnnotations().get(k).getTokens().size(); j++)
+					{int help=0;
+						if(s1.getSentence(i).getAnnotations().get(k).getType().toString()=="WEIGHT")
+						{
+							
+							if(help==0)
+							for(int l=0;l<g.length;l++)
+							{
+								if(g[l].equals(s1.getSentence(i).getAnnotations().get(k).getTokens().get(j)))
+								{
+									//System.out.println("satz"+i+g[l]+"und"+s1.getSentence(i).getAnnotations().get(k).getTokens().get(j));
+									help2="g";
+									help++;
+								}
+							}	
+							if(help==0)
+							for(int l=0;l<t.length;l++)
+							{	
+								if(t[l].equals(s1.getSentence(i).getAnnotations().get(k).getTokens().get(j)))
+								{
+									//System.out.println("satz"+i+"ton");
+									help++;
+									help2="t";
+								}
+							
+							}	
+							if(help==0)
+							for(int l=0;l<kg.length;l++)
+							{	
+								if(kg[l].equals(s1.getSentence(i).getAnnotations().get(k).getTokens().get(j)))
+								{
+									//System.out.println("satz"+i+"kg");
+									help++;
+									help2="kg";
+								}
+						
+							}
+						
+						}
+						else
+						{
+							
+							//abgleich mit längen string
+						}
+						
+						
+						if(help!=0)
+						{	BigDecimal b= new BigDecimal("10");
+						System.out.println(conversionStandard.get(help2));
+						b=b.add(conversionStandard.get(help2));
+							
+							
+						}	
+					}
+						
+						
+						
+						
+					}
+					
+					
+					
+		
+				
+				
+		}
 		
 	}
 }
