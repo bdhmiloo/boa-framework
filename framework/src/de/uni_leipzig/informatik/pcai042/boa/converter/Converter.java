8b3a4dc76da6a502506209082d0c4d38085266d8
boa.christians <boa.christians@localhost>
2012-05-04 20:55:39 +0200

    working on weight up to here

--- framework/src/de/uni_leipzig/informatik/pcai042/boa/converter/Converter.java
+++ framework/src/de/uni_leipzig/informatik/pcai042/boa/converter/Converter.java
@@ -27,7 +27,11 @@ import nu.xom.ValidityException;
 import de.uni_leipzig.informatik.pcai042.boa.manager.BoaAnnotation;
 import de.uni_leipzig.informatik.pcai042.boa.manager.ConfigLoader;
 import de.uni_leipzig.informatik.pcai042.boa.manager.SentenceLoader;
+import edu.stanford.nlp.io.EncodingPrintWriter.out;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 /**
  * 
  * @author Duc Huy Bui; Christian Kahmann
@@ -35,8 +39,10 @@ import de.uni_leipzig.informatik.pcai042.boa.manager.SentenceLoader;
 public abstract class Converter
 {
 	
-	private static HashMap<String, BigDecimal> conversionStandard;
-	private static HashMap<String, BigDecimal> conversionUnit;
+	private static HashMap<String, BigDecimal> conversionStandardw;
+	private static HashMap<String, BigDecimal> conversionUnitw;
+	private static HashMap<String, BigDecimal> conversionStandardl;
+	private static HashMap<String, BigDecimal> conversionUnitl;
 	/**
 	 * 
 	 * 
@@ -58,6 +64,28 @@ public abstract class Converter
 	 * @param annotation
 	 * @return
 	 */
+	protected static  boolean checkIfNumber(String token)
+	{	int help=0;
+		try
+		{
+			Integer.parseInt(token);
+			help++;
+		}
+		catch(NumberFormatException e){}
+		try
+		{
+			if(token.contains(",")) token = token.replace(",", ".");
+			Double.parseDouble(token);
+			help++;
+			
+		}
+		catch(NumberFormatException e){}
+		
+		if(help==0)		
+		return false;
+		else
+		return true;	
+	}
 	public ArrayList<String> convertUnit(BoaAnnotation annotation)
 	{
 		
@@ -71,14 +99,36 @@ public abstract class Converter
 	public static void main(String arg[])
 	{
 		
-		
-		
-		int test=0;
+		File file = new File("test.txt");
+        try {
+            System.setOut(new PrintStream(new FileOutputStream(file,true)));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+		int count=0;
+		String[] µm={"µm"};
+		String[] mm={"nm","nanometer","nanometers"};
+		String[] cm={"cm","centimeter","centimeters"};
+		String[] dm={"dm","decimeters","decimeter"};
+		String[] m={"m","meter","meters","metres"};
+		String[] km={"km","kilometer","kilometers"};
+		String[] ft={"ft","feet","feets","'"};
+		String[] in={"in","insh","inshes",};
+		String[] yard={"yard","yards","yd","yds"};
+		String[] seamile={"sm","seamile","seamiles","nautic mile","nautic miles"};
+		String[] mile={"mile","miles"};
+		String []µg={"µg","microgram","micrograms"};
+		String []mg={"mg","milligram","milligrams"};
+		String []Kt={"Kt","carat"};
+		String []lbs={"lbs","lb","labs","pound","pounds"};
+		String []oz={"oz","ounze"};
 		String []g={"g","gram"};
-		String []t={"t","ton","metric ton","metric tons","metric tons"};
-		String []kg={"kg","kilogram"};
+		String []t={"t","ton","metric ton","metric tons","tons"};
+		String []kg={"kg","kilogram","kilo","kg."};
+		String[] help3={"g","t","kg","oz","lbs","Kt","µg","mg"};
+		String[] help4={"µm","mm","cm","dm","m","km","mile","seamile","yard","ft","in"};
 		SentenceLoader s1;
-		double f;
+		
 		s1=null;
 		try
 		{
@@ -96,28 +146,29 @@ public abstract class Converter
 		
 		if (s1 == null)
 			return;
+		String [][] ausgabe=new String[s1.getSentenceCount()][20];
 		
-		
-		for (int i = 0; i < s1.getSentenceCount(); i++)
+		for (int i = 0; i <s1.getSentenceCount() ; i++)
 		{
 			
 			
 				for (int k = 0; k < s1.getSentence(i).getAnnotations().size(); k++)
 				{
 					String help2=null;
-					if(s1.getSentence(i).getAnnotations().get(k).getType().toString()!="DATE"&&s1.getSentence(i).getAnnotations().get(k).getType().toString()!="TEMPERATURE")
+					String help5=null;
+					if(s1.getSentence(i).getAnnotations().get(k).getType().toString()=="WEIGHT")
 					{	
-						conversionStandard  = new ConfigLoader().openConfigConversion("Standard", s1.getSentence(i).getAnnotations().get(k).getType().toString());
-						conversionUnit  = new ConfigLoader().openConfigConversion("Unit", s1.getSentence(i).getAnnotations().get(k).getType().toString());
-					}	
+						conversionStandardw  = new ConfigLoader().openConfigConversion("Standard", "WEIGHT");
+						conversionUnitw = new ConfigLoader().openConfigConversion("Unit", "WEIGHT");
+						
+						
 				
 					//System.out.println(conversionStandard);
 					for (int j = 0; j < s1.getSentence(i).getAnnotations().get(k).getTokens().size(); j++)
-					{int help=0;
-						if(s1.getSentence(i).getAnnotations().get(k).getType().toString()=="WEIGHT")
-						{
+					{	int help=0;
+						
 							
-							if(help==0)
+							
 							for(int l=0;l<g.length;l++)
 							{
 								if(g[l].equals(s1.getSentence(i).getAnnotations().get(k).getTokens().get(j)))
@@ -149,35 +200,397 @@ public abstract class Converter
 								}
 						
 							}
+							if(help==0)
+								for(int l=0;l<Kt.length;l++)
+								{	
+									if(Kt[l].equals(s1.getSentence(i).getAnnotations().get(k).getTokens().get(j)))
+									{
+										//System.out.println("satz"+i+"ton");
+										help++;
+										help2="Kt";
+									}
+								
+								}								
+							if(help==0)
+								for(int l=0;l<oz.length;l++)
+								{	
+									if(oz[l].equals(s1.getSentence(i).getAnnotations().get(k).getTokens().get(j)))
+									{
+										//System.out.println("satz"+i+"ton");
+										help++;
+										help2="oz";
+									}
+								
+								}	
+							if(help==0)
+								for(int l=0;l<µg.length;l++)
+								{	
+									if(µg[l].equals(s1.getSentence(i).getAnnotations().get(k).getTokens().get(j)))
+									{
+										//System.out.println("satz"+i+"ton");
+										help++;
+										help2="µg";
+									}
+								
+								}	
+							if(help==0)
+								for(int l=0;l<mg.length;l++)
+								{	
+									if(mg[l].equals(s1.getSentence(i).getAnnotations().get(k).getTokens().get(j)))
+									{
+										//System.out.println("satz"+i+"ton");
+										help++;
+										help2="mg";
+									}
+								
+								}	
+							if(help==0)
+								for(int l=0;l<lbs.length;l++)
+								{	
+									if(lbs[l].equals(s1.getSentence(i).getAnnotations().get(k).getTokens().get(j)))
+									{
+										//System.out.println("satz"+i+"ton");
+										help++;
+										help2="lbs";
+									}
+								
+								}	
+							
+							
+							
+								
 						
-						}
-						else
+					}
+						
+						
+					
+						
+						
+							
+							
+						
+						if(help2!=null)
 						{
-							
-							//abgleich mit längen string
-						}
+						//System.out.println(i+" "+k+" "+j+" "+help);
 						
+							double z=0;
+							
+							//System.out.println(conversionStandard.get(help2));
 						
-						if(help!=0)
-						{	BigDecimal b= new BigDecimal("10");
-						System.out.println(conversionStandard.get(help2));
-						b=b.add(conversionStandard.get(help2));
+							//System.out.println(i+" "+k+" "+j);
+							for(int w=0;w<s1.getSentence(i).getAnnotations().get(k).getTokens().size();w++)
+							{
+								if(checkIfNumber(s1.getSentence(i).getAnnotations().get(k).getTokens().get(w)))
+								{
+									String token=s1.getSentence(i).getAnnotations().get(k).getTokens().get(w);
+									if(token.contains("+"))
+										token=token.replace("+", "");
+									if(token.contains(","))
+										token = token.replace(",", ".");
+									if(token.contains("."))	
+										z=Double.parseDouble(token);
+									
+									else
+									{	
+										z=(double)Integer.parseInt(token);
+										
+									}
+								}
+							}	
 							
 							
-						}	
+							
+								BigDecimal a= new BigDecimal(z);
+							BigDecimal standard=a.multiply(conversionStandardw.get(help2));
+							
+							ausgabe[i][k]="satz"+i+" annotation"+k+":";
+								
+							for(int d=0;d<help3.length;d++)
+							{
+								BigDecimal neu=standard.multiply(conversionUnitw.get(help3[d]));
+								
+								if(help3[d].equals("kg"))
+								{	
+									for(int l=0;l<kg.length;l++)
+									ausgabe[i][k]=ausgabe[i][k].concat(neu+" "+kg[l]+"  //  ");
+									count++;
+								}
+								if(help3[d].equals("g"))
+								{	
+									for(int l=0;l<g.length;l++)
+									ausgabe[i][k]=ausgabe[i][k].concat(neu+" "+g[l]+"  //  ");
+									count++;
+								}
+								if(help3[d].equals("t"))
+								{	
+									for(int l=0;l<t.length;l++)
+									ausgabe[i][k]=ausgabe[i][k].concat(neu+" "+t[l]+"  //  ");
+									count++;
+								}
+								if(help3[d].equals("Kt"))
+								{	
+									for(int l=0;l<Kt.length;l++)
+									ausgabe[i][k]=ausgabe[i][k].concat(neu+" "+Kt[l]+"  //  ");
+									count++;
+								}
+								if(help3[d].equals("oz"))
+								{	
+									for(int l=0;l<oz.length;l++)
+									ausgabe[i][k]=ausgabe[i][k].concat(neu+" "+oz[l]+"  //  ");
+									count++;
+								}
+								if(help3[d].equals("lbs"))
+								{	
+									for(int l=0;l<lbs.length;l++)
+									ausgabe[i][k]=ausgabe[i][k].concat(neu+" "+lbs[l]+"  //  ");
+									count++;
+								}
+								if(help3[d].equals("mg"))
+								{	
+									for(int l=0;l<mg.length;l++)
+									ausgabe[i][k]=ausgabe[i][k].concat(neu+" "+mg[l]+"  //  ");
+									count++;
+								}
+								if(help3[d].equals("µg"))
+								{	
+									for(int l=0;l<µg.length;l++)
+									ausgabe[i][k]=ausgabe[i][k].concat(neu+" "+µg[l]+"  //  ");
+									count++;
+								}
+							}
+														
+							
+								
+							}
+								
+								
+													
+								
+							}
+					if(s1.getSentence(i).getAnnotations().get(k).getType().toString()=="LINEAR_MEASURE")
+					{	
+						conversionStandardl  = new ConfigLoader().openConfigConversion("Standard", "LINEAR_MEASURE");
+						conversionUnitl = new ConfigLoader().openConfigConversion("Unit", "LINEAR_MEASURE");
+						System.out.print(conversionStandardl.get("m"));
+						
+				
+					//System.out.println(conversionStandard);
+					for (int j = 0; j < s1.getSentence(i).getAnnotations().get(k).getTokens().size(); j++)
+					{	int help=0;
+						
+							
+							
+							for(int l=0;l<g.length;l++)
+							{
+								if(g[l].equals(s1.getSentence(i).getAnnotations().get(k).getTokens().get(j)))
+								{
+									//System.out.println("satz"+i+g[l]+"und"+s1.getSentence(i).getAnnotations().get(k).getTokens().get(j));
+									help2="g";
+									help++;
+								}
+							}	
+							if(help==0)
+							for(int l=0;l<t.length;l++)
+							{	
+								if(t[l].equals(s1.getSentence(i).getAnnotations().get(k).getTokens().get(j)))
+								{
+									//System.out.println("satz"+i+"ton");
+									help++;
+									help2="t";
+								}
+							
+							}	
+							if(help==0)
+							for(int l=0;l<kg.length;l++)
+							{	
+								if(kg[l].equals(s1.getSentence(i).getAnnotations().get(k).getTokens().get(j)))
+								{
+									//System.out.println("satz"+i+"kg");
+									help++;
+									help2="kg";
+								}
+						
+							}
+							if(help==0)
+								for(int l=0;l<Kt.length;l++)
+								{	
+									if(Kt[l].equals(s1.getSentence(i).getAnnotations().get(k).getTokens().get(j)))
+									{
+										//System.out.println("satz"+i+"ton");
+										help++;
+										help2="Kt";
+									}
+								
+								}								
+							if(help==0)
+								for(int l=0;l<oz.length;l++)
+								{	
+									if(oz[l].equals(s1.getSentence(i).getAnnotations().get(k).getTokens().get(j)))
+									{
+										//System.out.println("satz"+i+"ton");
+										help++;
+										help2="oz";
+									}
+								
+								}	
+							if(help==0)
+								for(int l=0;l<µg.length;l++)
+								{	
+									if(µg[l].equals(s1.getSentence(i).getAnnotations().get(k).getTokens().get(j)))
+									{
+										//System.out.println("satz"+i+"ton");
+										help++;
+										help2="µg";
+									}
+								
+								}	
+							if(help==0)
+								for(int l=0;l<mg.length;l++)
+								{	
+									if(mg[l].equals(s1.getSentence(i).getAnnotations().get(k).getTokens().get(j)))
+									{
+										//System.out.println("satz"+i+"ton");
+										help++;
+										help2="mg";
+									}
+								
+								}	
+							if(help==0)
+								for(int l=0;l<lbs.length;l++)
+								{	
+									if(lbs[l].equals(s1.getSentence(i).getAnnotations().get(k).getTokens().get(j)))
+									{
+										//System.out.println("satz"+i+"ton");
+										help++;
+										help2="lbs";
+									}
+								
+								}	
+							
+							
+							
+								
+						
 					}
 						
 						
+					
 						
 						
-					}
+							
+							
+						
+						if(help2!=null)
+						{
+						//System.out.println(i+" "+k+" "+j+" "+help);
+						
+							double z=0;
+							
+							//System.out.println(conversionStandard.get(help2));
+						
+							//System.out.println(i+" "+k+" "+j);
+							for(int w=0;w<s1.getSentence(i).getAnnotations().get(k).getTokens().size();w++)
+							{
+								if(checkIfNumber(s1.getSentence(i).getAnnotations().get(k).getTokens().get(w)))
+								{
+									String token=s1.getSentence(i).getAnnotations().get(k).getTokens().get(w);
+									if(token.contains("+"))
+										token=token.replace("+", "");
+									if(token.contains(","))
+										token = token.replace(",", ".");
+									if(token.contains("."))	
+										z=Double.parseDouble(token);
+									
+									else
+									{	
+										z=(double)Integer.parseInt(token);
+										
+									}
+								}
+							}	
+							
+							
+							
+								BigDecimal a= new BigDecimal(z);
+							BigDecimal standard=a.multiply(conversionStandardw.get(help2));
+							
+							ausgabe[i][k]="satz"+i+" annotation"+k+":";
+								
+							for(int d=0;d<help3.length;d++)
+							{
+								BigDecimal neu=standard.multiply(conversionUnitw.get(help3[d]));
+								
+								if(help3[d].equals("kg"))
+								{	
+									for(int l=0;l<kg.length;l++)
+									ausgabe[i][k]=ausgabe[i][k].concat(neu+" "+kg[l]+"  //  ");
+									count++;
+								}
+								if(help3[d].equals("g"))
+								{	
+									for(int l=0;l<g.length;l++)
+									ausgabe[i][k]=ausgabe[i][k].concat(neu+" "+g[l]+"  //  ");
+									count++;
+								}
+								if(help3[d].equals("t"))
+								{	
+									for(int l=0;l<t.length;l++)
+									ausgabe[i][k]=ausgabe[i][k].concat(neu+" "+t[l]+"  //  ");
+									count++;
+								}
+								if(help3[d].equals("Kt"))
+								{	
+									for(int l=0;l<Kt.length;l++)
+									ausgabe[i][k]=ausgabe[i][k].concat(neu+" "+Kt[l]+"  //  ");
+									count++;
+								}
+								if(help3[d].equals("oz"))
+								{	
+									for(int l=0;l<oz.length;l++)
+									ausgabe[i][k]=ausgabe[i][k].concat(neu+" "+oz[l]+"  //  ");
+									count++;
+								}
+								if(help3[d].equals("lbs"))
+								{	
+									for(int l=0;l<lbs.length;l++)
+									ausgabe[i][k]=ausgabe[i][k].concat(neu+" "+lbs[l]+"  //  ");
+									count++;
+								}
+								if(help3[d].equals("mg"))
+								{	
+									for(int l=0;l<mg.length;l++)
+									ausgabe[i][k]=ausgabe[i][k].concat(neu+" "+mg[l]+"  //  ");
+									count++;
+								}
+								if(help3[d].equals("µg"))
+								{	
+									for(int l=0;l<µg.length;l++)
+									ausgabe[i][k]=ausgabe[i][k].concat(neu+" "+µg[l]+"  //  ");
+									count++;
+								}
+							}
+														
+							
+								
+							}
+								
+								
+													
+								
+							}
+							
 					
 					
+			
+						
+						
 					
-		
+			 
+			        
+					System.out.println(ausgabe[i][k]);	
 				
+					
 				
-		}
-		
 	}
-}
+}System.out.println(count);
+		}}
