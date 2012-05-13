7f5ed52002ef6d70c49d12d706ebbe2728259038
Duc Huy Bui <bdh.coding@googlemail.com>
2012-05-13 12:57:32 +0200

    fixed some stupid probs; date patterns similiar to "13.05.2012" seem to work now; to do: write difficult algo for dates consisting of strings like "3rd May, 2012"

---
+++ framework/src/de/uni_leipzig/informatik/pcai042/boa/manager/Annotater.java
@@ -0,0 +1,95 @@
+/*
+ * Annotater.java
+ * 
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 3 of the License, or (at your
+ * option) any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.uni_leipzig.informatik.pcai042.boa.manager;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Set;
+
+import de.uni_leipzig.informatik.pcai042.boa.manager.BoaAnnotation.Type;
+import de.uni_leipzig.informatik.pcai042.boa.searcher.LabelSearcher;
+import de.uni_leipzig.informatik.pcai042.boa.searcher.SearchAlgorithm;
+
+/**
+ * Creates LabelSearchers for each Unit we want to find and makes the process
+ * on a given ArrayList of BoaSentence. Both the name of the units and the algorithm
+ * used to search for it are stored in the sForms.properties file.
+ * @author Jakob
+ *
+ */
+
+public class Annotater
+{
+	private boolean isInitialized = false;
+	private ArrayList<LabelSearcher> searchers;
+	
+	public Annotater()
+	{
+		try
+		{
+			//DynamicEnumTest eTest;
+			String algoName, currentUnit;
+			searchers = new ArrayList<LabelSearcher>();
+			ConfigLoader loader = new ConfigLoader();
+			Set<String> units = loader.openConfigSurfaceForms("ALL_UNITS");
+			Iterator<String> itUnit = units.iterator();
+			
+			while(itUnit.hasNext())
+			{
+				currentUnit = itUnit.next();
+				algoName = loader.returnValue(currentUnit.toUpperCase()+"_ALGO").toString();
+				Class algo = Class.forName(algoName);
+				
+				SearchAlgorithm sAlgo = (SearchAlgorithm) algo.newInstance();
+				System.out.println(sAlgo);
+				
+				//if(Type.valueOf(currentUnit.toUpperCase())==null) eTest.add
+				searchers.add(new LabelSearcher(Type.valueOf(currentUnit.toUpperCase()), sAlgo));
+			}
+			
+			isInitialized = true;
+		}
+		catch(ClassNotFoundException e)
+		{
+			//create logger message
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public void annotate(ArrayList<BoaSentence> sentences)
+	{
+		if(!isInitialized) return;
+		
+		BoaSentence currentSentence;
+		
+		Iterator<BoaSentence> sentenceIt = sentences.iterator();
+		
+		while(sentenceIt.hasNext())
+		{
+			currentSentence = sentenceIt.next();
+			Iterator<LabelSearcher> searchIt = searchers.iterator();
+			
+			while(searchIt.hasNext())
+			{
+				searchIt.next().searchUnit(currentSentence);
+			}
+		}
+
+	}
+}
---
+++ framework/src/de/uni_leipzig/informatik/pcai042/boa/searcher/LabelSearcher.java
@@ -0,0 +1,67 @@
+/*
+ * LabelSearcher.java
+ * 
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 3 of the License, or (at your
+ * option) any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.uni_leipzig.informatik.pcai042.boa.searcher;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import de.uni_leipzig.informatik.pcai042.boa.manager.BoaSentence;
+import de.uni_leipzig.informatik.pcai042.boa.manager.ConfigLoader;
+import de.uni_leipzig.informatik.pcai042.boa.manager.BoaAnnotation.Type;
+
+public class LabelSearcher
+{
+	private SearchAlgorithm algo;
+	
+	public LabelSearcher(Type annoType, SearchAlgorithm algorithm)
+	{
+		try
+		{
+		    ConfigLoader load = new ConfigLoader();
+			Set<String> unitNames = load.openConfigSurfaceForms(annoType.toString());
+			Set<String> surfaceForms = new HashSet<String>();
+			
+			Iterator<String> it = unitNames.iterator();
+			while(it.hasNext())
+			{
+				surfaceForms.addAll(load.openConfigSurfaceForms(it.next().toUpperCase()));
+			}
+			
+			algo = algorithm.getClass().newInstance();
+			algo.setSurForms(surfaceForms);
+			algo.setAnnoType(annoType);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	public void searchUnit(BoaSentence sentence)
+	{
+		try
+		{
+			algo.search(sentence);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+		return;
+	}
+}
